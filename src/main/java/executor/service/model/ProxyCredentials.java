a065b1f508e36b3310eef4fe14adda3f88a71417
Oleksii <mail.alexbondarenko@gmail.com>
2023-08-01 14:43:59 +0300

    add ProxyCredentials DTO

---
+++ src/main/java/executor/service/model/ProxyCredentials.java
@@ -0,0 +1,40 @@
+package executor.service.model;
+
+import java.util.Objects;
+
+public class ProxyCredentials {
+    private String username;
+    private String password;
+
+    public ProxyCredentials() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProxyCredentials that = (ProxyCredentials) o;
+        return Objects.equals(username, that.username) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+}
