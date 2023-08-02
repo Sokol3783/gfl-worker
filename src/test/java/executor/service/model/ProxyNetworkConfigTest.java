343e7d5fbdb91ea23c69683c0b127b37f62c1080
Alexander Antonenko <mercurialf3@gmail.com>
2023-08-03 00:53:50 +0300

    Fixes

--- src/test/java/executor/service/model/ProxyNetworkConfigTest.java
+++ src/test/java/executor/service/model/ProxyNetworkConfigTest.java
@@ -1,5 +1,6 @@
 package executor.service.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,14 +8,15 @@ import static org.junit.Assert.*;
 public class ProxyNetworkConfigTest {
     private static final String HOSTNAME = "example.com";
     private static final Integer PORT = 8080;
-    private static final ProxyNetworkConfig CONFIG1;
-    private static final ProxyNetworkConfig CONFIG2;
-    private static final ProxyNetworkConfig CONFIG3;
+    private ProxyNetworkConfig config1;
+    private ProxyNetworkConfig config2;
+    private ProxyNetworkConfig config3;
 
-    static {
-        CONFIG1 = new ProxyNetworkConfig("example.com", 8080);
-        CONFIG2 = new ProxyNetworkConfig("example.com", 8080);
-        CONFIG3 = new ProxyNetworkConfig("example.org", 8081);
+    @Before
+    public void setUp() {
+        config1 = new ProxyNetworkConfig("example.com", 8080);
+        config2 = new ProxyNetworkConfig("example.com", 8080);
+        config3 = new ProxyNetworkConfig("example.org", 8081);
     }
 
     @Test
@@ -33,13 +35,13 @@ public class ProxyNetworkConfigTest {
 
     @Test
     public void testEquals() {
-        assertEquals(CONFIG1, CONFIG2);
-        assertNotEquals(CONFIG1, CONFIG3);
+        assertEquals(config1, config2);
+        assertNotEquals(config1, config3);
     }
 
     @Test
     public void testHashCode() {
-        assertEquals(CONFIG1.hashCode(), CONFIG2.hashCode());
-        assertNotEquals(CONFIG1.hashCode(), CONFIG3.hashCode());
+        assertEquals(config1.hashCode(), config2.hashCode());
+        assertNotEquals(config1.hashCode(), config3.hashCode());
     }
 }
