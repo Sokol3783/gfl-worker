78775412c7030645735a75da2738ee799a85e9dd
Alexander Antonenko <mercurialf3@gmail.com>
2023-08-02 15:58:50 +0300

    Extracted constants, split the test into two separate ones, removed checks for getters and setters

--- src/test/java/executor/service/model/ProxyNetworkConfigTest.java
+++ src/test/java/executor/service/model/ProxyNetworkConfigTest.java
@@ -5,6 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ProxyNetworkConfigTest {
+    private static final String HOSTNAME = "example.com";
+    private static final Integer PORT = 8080;
+    private static final ProxyNetworkConfig CONFIG1;
+    private static final ProxyNetworkConfig CONFIG2;
+    private static final ProxyNetworkConfig CONFIG3;
+
+    static {
+        CONFIG1 = new ProxyNetworkConfig("example.com", 8080);
+        CONFIG2 = new ProxyNetworkConfig("example.com", 8080);
+        CONFIG3 = new ProxyNetworkConfig("example.org", 8081);
+    }
 
     @Test
     public void testEmptyConstructor() {
@@ -15,34 +26,20 @@ public class ProxyNetworkConfigTest {
 
     @Test
     public void testParameterizedConstructor() {
-        String hostname = "example.com";
-        Integer port = 8080;
-        ProxyNetworkConfig config = new ProxyNetworkConfig(hostname, port);
-        assertEquals(hostname, config.getHostname());
-        assertEquals(port, config.getPort());
+        ProxyNetworkConfig config = new ProxyNetworkConfig(HOSTNAME, PORT);
+        assertEquals(HOSTNAME, config.getHostname());
+        assertEquals(PORT, config.getPort());
     }
 
     @Test
-    public void testGettersAndSetters() {
-        ProxyNetworkConfig config = new ProxyNetworkConfig();
-        String hostname = "proxy.example.com";
-        Integer port = 8888;
-        config.setHostname(hostname);
-        config.setPort(port);
-
-        assertEquals(hostname, config.getHostname());
-        assertEquals(port, config.getPort());
+    public void testEquals() {
+        assertEquals(CONFIG1, CONFIG2);
+        assertNotEquals(CONFIG1, CONFIG3);
     }
 
     @Test
-    public void testEqualsAndHashCode() {
-        ProxyNetworkConfig config1 = new ProxyNetworkConfig("example.com", 8080);
-        ProxyNetworkConfig config2 = new ProxyNetworkConfig("example.com", 8080);
-        ProxyNetworkConfig config3 = new ProxyNetworkConfig("example.org", 8081);
-
-        assertEquals(config1, config2);
-        assertNotEquals(config1, config3);
-        assertEquals(config1.hashCode(), config2.hashCode());
-        assertNotEquals(config1.hashCode(), config3.hashCode());
+    public void testHashCode() {
+        assertEquals(CONFIG1.hashCode(), CONFIG2.hashCode());
+        assertNotEquals(CONFIG1.hashCode(), CONFIG3.hashCode());
     }
 }
