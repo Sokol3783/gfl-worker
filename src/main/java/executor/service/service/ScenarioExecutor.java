90423ed619e97e521eb9a1d532e69c017f14648d
Alexander Tuleninov <talextu@gmail.com>
2023-08-08 14:13:42 +0300

    changed ScenarioExecutor.java

--- src/main/java/executor/service/service/ScenarioExecutor.java
+++ src/main/java/executor/service/service/ScenarioExecutor.java
@@ -3,8 +3,9 @@ package executor.service.service;
 import executor.service.model.Scenario;
 import org.openqa.selenium.WebDriver;
 
-public interface ScenarioExecutor {
+public interface ScenarioExecutor extends ExecutionService {
 
+    @Override
     void execute(Scenario scenario, WebDriver webDriver);
 
 }
