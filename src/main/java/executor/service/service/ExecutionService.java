13a4151a05d77c28fb6b0b29a93d314832347c73
Alexander Tuleninov <talextu@gmail.com>
2023-08-08 12:08:24 +0300

    fixed void execute(Scenario scenario, WebDriver webDriver);

--- src/main/java/executor/service/service/ExecutionService.java
+++ src/main/java/executor/service/service/ExecutionService.java
@@ -1,12 +1,11 @@
 package executor.service.service;
 
+import executor.service.model.Scenario;
 import org.openqa.selenium.WebDriver;
 
 public class ExecutionService {
 
-    public void execute(WebDriver webDriver,
-                        ScenarioSourceListener scenarioSourceListener,
-                        ScenarioExecutor scenarioExecutor) {
+    public void execute(Scenario scenario, WebDriver webDriver) {
 
     }
 }
