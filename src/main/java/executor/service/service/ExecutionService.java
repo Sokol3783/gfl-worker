fa823fd15f98c71104b3dd7c5c194d5fac1cf15b
Alexander Tuleninov <talextu@gmail.com>
2023-08-08 12:54:04 +0300

    changed ExectionService.java on the interface

--- src/main/java/executor/service/service/ExecutionService.java
+++ src/main/java/executor/service/service/ExecutionService.java
@@ -3,9 +3,8 @@ package executor.service.service;
 import executor.service.model.Scenario;
 import org.openqa.selenium.WebDriver;
 
-public class ExecutionService {
+public interface ExecutionService {
 
-    public void execute(Scenario scenario, WebDriver webDriver) {
+    void execute(Scenario scenario, WebDriver webDriver);
 
-    }
 }
