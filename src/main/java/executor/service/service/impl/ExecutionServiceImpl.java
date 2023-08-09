e3578c2dbdb8fc0190dcc017a33db315db2c9664
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2023-08-09 15:53:20 +0300

    add impl for interface services

---
+++ src/main/java/executor/service/service/impl/ExecutionServiceImpl.java
@@ -0,0 +1,12 @@
+package executor.service.service.impl;
+
+import executor.service.model.Scenario;
+import executor.service.service.ExecutionService;
+import org.openqa.selenium.WebDriver;
+
+public class ExecutionServiceImpl implements ExecutionService {
+    @Override
+    public void execute(Scenario scenario, WebDriver webDriver) {
+
+    }
+}
---
+++ src/main/java/executor/service/service/impl/StepExecutionClickXpathImpl.java
@@ -0,0 +1,17 @@
+package executor.service.service.impl;
+
+import executor.service.model.Step;
+import executor.service.service.StepExecutionClickXpath;
+import org.openqa.selenium.WebDriver;
+
+public class StepExecutionClickXpathImpl implements StepExecutionClickXpath {
+    @Override
+    public String getStepAction() {
+        return null;
+    }
+
+    @Override
+    public void step(WebDriver webDriver, Step step) {
+
+    }
+}
