6e808e4ed1a3accd4b362e709a7dc263c389ebc9
Alexander Tuleninov <talextu@gmail.com>
2023-08-09 15:33:11 +0300

    coding ParalleFlowExecutorService structure

--- src/main/java/executor/service/service/ParalleFlowExecutorService.java
+++ src/main/java/executor/service/service/ParalleFlowExecutorService.java
@@ -1,7 +1,23 @@
 package executor.service.service;
 
+import executor.service.model.Scenario;
+import org.openqa.selenium.WebDriver;
+
 public class ParalleFlowExecutorService {
 
+    private final WebDriver webDriver;
+    private final ExecutionService executionService;
 
+    public ParalleFlowExecutorService(WebDriver webDriver, ExecutionService executionService) {
+        this.webDriver = webDriver;
+        this.executionService = executionService;
+    }
 
+    public void addScenario(Scenario scenario) {
+
+    }
+
+    public void runAllScenario() {
+
+    }
 }
