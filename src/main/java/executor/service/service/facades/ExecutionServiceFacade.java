4c8b0a836815d6321b1035e43f994c3fa36b677c
IfLunatic <dimasilenkoluna@gmail.com>
2023-08-10 12:21:31 +0300

    added ExecutionServiceFacade

---
+++ src/main/java/executor/service/service/facades/ExecutionServiceFacade.java
@@ -0,0 +1,29 @@
+package executor.service.service.facades;
+
+import executor.service.model.Scenario;
+import executor.service.service.ScenarioExecutor;
+import executor.service.service.ScenarioSourceListener;
+import executor.service.service.WebDriverInitializer;
+import org.openqa.selenium.WebDriver;
+
+public class ExecutionServiceFacade {
+
+    private WebDriverInitializer driverInitializer;
+    private ScenarioExecutor scenarioExecutor;
+    private ScenarioSourceListener scenarioSourceListener;
+
+    public ExecutionServiceFacade(WebDriverInitializer driverInitializer, ScenarioExecutor scenarioExecutor, ScenarioSourceListener scenarioSourceListener) {
+        this.driverInitializer = driverInitializer;
+        this.scenarioExecutor = scenarioExecutor;
+        this.scenarioSourceListener = scenarioSourceListener;
+    }
+
+    public void executeScenario() {
+        WebDriver webDriver = driverInitializer.getInstance();
+        Scenario scenario =  scenarioSourceListener.execute();
+
+        if (scenario != null) {
+            scenarioExecutor.execute(scenario, webDriver);
+        }
+    }
+}
