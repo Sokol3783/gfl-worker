f0524329a808d018c00968c8ab80d678881b52ff
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2023-08-10 12:32:40 +0300

    write FactoryDI class

--- src/main/java/executor/service/service/FactoryForDI.java
+++ src/main/java/executor/service/service/FactoryForDI.java
@@ -1,4 +1,65 @@
 package executor.service.service;
 
+import executor.service.service.impl.*;
+
 public class FactoryForDI {
+    private final WebDriverInitializer webDriverInitializer;
+    private final ProxySourcesClient proxySourcesClient;
+    private final ScenarioSourceListener scenarioSourceListener;
+    private final ParalleFlowExecutorService paralleFlowExecutorService;
+    private final ExecutionService executionService;
+    private final ScenarioExecutor scenarioExecutor;
+    private final StepExecutionSleep stepExecutionSleep;
+    private final StepExecutionClickXpath stepExecutionClickXpath;
+
+    private final StepExecutionClickCss stepExecutionClickCss;
+
+    public FactoryForDI() {
+        webDriverInitializer = new WebDriverInitializer();
+        proxySourcesClient = new ProxySourcesClientImpl();
+        scenarioSourceListener = new ScenarioSourceListenerImpl();
+        paralleFlowExecutorService = new ParalleFlowExecutorService();
+        executionService = new ExecutionServiceImpl();
+        scenarioExecutor = new ScenarioExecutorImpl();
+        stepExecutionSleep = new StepExecutionSleepImpl();
+        stepExecutionClickCss = new StepExecutionClickCssImpl();
+        stepExecutionClickXpath = new StepExecutionClickXpathImpl();
+    }
+
+    public WebDriverInitializer getWebDriverInitializer() {
+        return webDriverInitializer;
+    }
+
+    public ProxySourcesClient getProxySourcesClient() {
+        return proxySourcesClient;
+    }
+
+    public ScenarioSourceListener getScenarioSourceListener() {
+        return scenarioSourceListener;
+    }
+
+    public ParalleFlowExecutorService getParalleFlowExecutorService() {
+        return paralleFlowExecutorService;
+    }
+
+    public ExecutionService getExecutionService() {
+        return executionService;
+    }
+
+    public ScenarioExecutor getScenarioExecutor() {
+        return scenarioExecutor;
+    }
+
+    public StepExecutionSleep getStepExecutionSleep() {
+        return stepExecutionSleep;
+    }
+
+    public StepExecutionClickXpath getStepExecutionClickXpath() {
+        return stepExecutionClickXpath;
+    }
+
+    public StepExecutionClickCss getStepExecutionClickCss() {
+        return stepExecutionClickCss;
+    }
+
 }
