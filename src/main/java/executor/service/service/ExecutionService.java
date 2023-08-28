a5ea943032ee9b576ecaca6e9cec09fdb9d784ed
Alexander Tuleninov <talextu@gmail.com>
2023-08-28 16:00:41 +0300

    refactor ExecutionService.java

--- src/main/java/executor/service/service/ExecutionService.java
+++ src/main/java/executor/service/service/ExecutionService.java
@@ -1,14 +1,30 @@
 package executor.service.service;
 
+import executor.service.model.Scenario;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+
 public class ExecutionService {
 
     private final ScenarioSourceListener scenarioSourceListener;
-
-    public ExecutionService(ScenarioSourceListener scenarioSourceListener) {
+    private final ScenarioExecutor scenarioExecutor;
+    public ExecutionService(ScenarioSourceListener scenarioSourceListener,
+                            ScenarioExecutor scenarioExecutor) {
         this.scenarioSourceListener = scenarioSourceListener;
+        this.scenarioExecutor = scenarioExecutor;
     }
 
     public void execute() {
-        scenarioSourceListener.execute();
+        WebDriver webDriver = getWebDriverPrototype();
+
+        List<Scenario> scenarios = scenarioSourceListener.execute();
+
+        scenarios.forEach(scenario -> scenarioExecutor.execute(scenario, webDriver));
+    }
+
+    private WebDriver getWebDriverPrototype() {
+        WebDriverInitializer webDriverInitializer = new WebDriverInitializer();
+        return webDriverInitializer.getInstance();
     }
 }
--- src/main/java/executor/service/service/ScenarioSourceListener.java
+++ src/main/java/executor/service/service/ScenarioSourceListener.java
@@ -1,7 +1,11 @@
 package executor.service.service;
 
+import executor.service.model.Scenario;
+
+import java.util.List;
+
 public interface ScenarioSourceListener {
 
-    void execute();
+    List<Scenario> execute();
 
 }
