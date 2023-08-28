9790c5c2681800644fae09de3990c80d52dad5a3
Alexander Tuleninov <talextu@gmail.com>
2023-08-28 18:19:09 +0300

    checking WebDriver for null

--- src/main/java/executor/service/service/ExecutionService.java
+++ src/main/java/executor/service/service/ExecutionService.java
@@ -1,11 +1,17 @@
 package executor.service.service;
 
+import executor.service.config.properties.PropertiesConfig;
 import executor.service.model.Scenario;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class ExecutionService {
+
+    private static final Logger log = LoggerFactory.getLogger(ExecutionService.class);
+
 
     private final ScenarioSourceListener scenarioSourceListener;
     private final ScenarioExecutor scenarioExecutor;
@@ -17,6 +23,10 @@ public class ExecutionService {
 
     public void execute() {
         WebDriver webDriver = getWebDriverPrototype();
+        if (webDriver == null) {
+            log.error("WebDriver cannot be null");
+            throw new IllegalArgumentException("WebDriver cannot be null");
+        }
 
         List<Scenario> scenarios = scenarioSourceListener.execute();
 
