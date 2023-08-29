e507457bcd1fe4efde33ad79c6e1606b76b24aab
Oleksii <mail.alexbondarenko@gmail.com>
2023-08-29 16:06:22 +0300

    add webDriverInitializer

--- src/main/java/executor/service/service/WebDriverInitializer.java
+++ src/main/java/executor/service/service/WebDriverInitializer.java
@@ -4,17 +4,6 @@ import executor.service.model.ProxyConfigHolder;
 import executor.service.model.WebDriverConfig;
 import org.openqa.selenium.WebDriver;
 
-public class WebDriverInitializer {
-    private WebDriverConfig webDriverConfig;
-    private ProxyConfigHolder proxyConfigHolder;
-    private WebDriver driver;
-
-    public WebDriverInitializer(WebDriverConfig webDriverConfig, ProxyConfigHolder proxyConfigHolder) {
-        this.webDriverConfig = webDriverConfig;
-        this.proxyConfigHolder = proxyConfigHolder;
-    }
-
-    public WebDriver getInstance() {
-        return driver;
-    }
+public interface WebDriverInitializer {
+    WebDriver getInstance(WebDriverConfig webDriverConfig, ProxyConfigHolder proxyConfigHolder);
 }
