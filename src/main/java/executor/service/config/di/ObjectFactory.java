9a72d8d608159253bb7ffe7e33a3c88cbea4cec1
Sokol3783 <sokolplus3783@gmail.com>
2023-08-31 14:17:45 +0300

    remove infrastructure logic from constructor

--- src/main/java/executor/service/config/di/ObjectFactory.java
+++ src/main/java/executor/service/config/di/ObjectFactory.java
@@ -1,55 +1,51 @@
 package executor.service.config.di;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ObjectFactory {
 
-    private static final Logger log = LoggerFactory.getLogger(ObjectFactory.class);
+  private static final Logger log = LoggerFactory.getLogger(ObjectFactory.class);
 
-    private final ApplicationContext context;
-    private final List<ObjectConfigurator> configurators = new ArrayList<>();
+  private final ApplicationContext context;
+  private final List<ObjectConfigurator> configurators;
 
-    public ObjectFactory(ApplicationContext context) {
-        this.context = context;
-        for (Class<? extends ObjectConfigurator> aClass : context.getConfig().getScanner().getSubTypesOf(ObjectConfigurator.class)) {
-            configurators.add(getAClass(aClass));
-        }
+  public ObjectFactory(ApplicationContext context, List<ObjectConfigurator> configurators) {
+    this.context = context;
+    this.configurators = configurators;
+  }
+
+  public <T> T createObject(Class<T> implClass) {
+    T t = create(implClass);
+
+    configure(t);
+
+    return t;
+  }
+
+  private ObjectConfigurator getAClass(Class<? extends ObjectConfigurator> aClass) {
+    try {
+      return aClass.getDeclaredConstructor().newInstance();
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+             NoSuchMethodException e) {
+      log.info("Error creating ObjectConfigurator instance for class: " + aClass.getName());
+      throw new RuntimeException(e);
     }
+  }
 
-    public <T> T createObject(Class<T> implClass) {
-        T t = create(implClass);
-
-        configure(t);
-
-        return t;
+  private <T> T create(Class<T> implClass) {
+    try {
+      return implClass.getDeclaredConstructor().newInstance();
+    } catch (InstantiationException | IllegalAccessException
+             | InvocationTargetException | NoSuchMethodException e) {
+      log.info("Error creating instance of class: " + implClass.getName());
+      throw new RuntimeException(e);
     }
+  }
 
-    private ObjectConfigurator getAClass(Class<? extends ObjectConfigurator> aClass) {
-        try {
-            return aClass.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                 NoSuchMethodException e) {
-            log.info("Error creating ObjectConfigurator instance for class: " + aClass.getName());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private <T> T create(Class<T> implClass) {
-        try {
-            return implClass.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException
-                 | InvocationTargetException | NoSuchMethodException e) {
-            log.info("Error creating instance of class: " + implClass.getName());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private <T> void configure(T t) {
-        configurators.forEach(objectConfigurator -> objectConfigurator.configure(t, context));
-    }
+  private <T> void configure(T t) {
+    configurators.forEach(objectConfigurator -> objectConfigurator.configure(t));
+  }
 }
