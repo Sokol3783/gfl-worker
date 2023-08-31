464df100f5b43508ee39ee0c956385a470c66940
Sokol3783 <sokolplus3783@gmail.com>
2023-08-31 13:53:10 +0300

    temp changes

--- src/main/java/executor/service/service/ParallelFlowExecutorService.java
+++ src/main/java/executor/service/service/ParallelFlowExecutorService.java
@@ -1,22 +1,21 @@
 package executor.service.service;
 
-import executor.service.model.Scenario;
 import org.openqa.selenium.WebDriver;
 
-public class ParalleFlowExecutorService {
+public class ParallelFlowExecutorService {
 
     private  WebDriver webDriver;
     private  ExecutionService executionService;
 
-    public ParalleFlowExecutorService() {
+    public ParallelFlowExecutorService() {
     }
 
-    public ParalleFlowExecutorService(WebDriver webDriver, ExecutionService executionService) {
+    public ParallelFlowExecutorService(WebDriver webDriver, ExecutionService executionService) {
         this.webDriver = webDriver;
         this.executionService = executionService;
     }
 
-    public void run(Scenario scenario) {
+    public void run() {
 
     }
 }
