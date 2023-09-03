a3c7b0902e9b81d2edaf4998b6addefd4e225535
Olexandr Tuleninov <talextu1@gmail.com>
2023-09-03 15:43:19 +0300

    added JavaDoc to private void await()

--- src/main/java/executor/service/service/parallel/ParalleFlowExecutorService.java
+++ src/main/java/executor/service/service/parallel/ParalleFlowExecutorService.java
@@ -56,6 +56,9 @@ public class ParalleFlowExecutorService {
         threadPoolExecutor.shutdown();
     }
 
+    /**
+     * Wait for the Workers threads to complete.
+     */
     private void await() {
         try {
             cdlParallelFlow.await();
