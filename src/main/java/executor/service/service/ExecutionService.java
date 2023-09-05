cc11ba1d0192ac7118bbf29c42e90e1e7160ee1d
Olexandr Tuleninov <talextu1@gmail.com>
2023-09-05 15:09:49 +0300

    added ExecutionService interface and waiting for tasks in BlockingQueue

--- src/main/java/executor/service/service/ExecutionService.java
+++ src/main/java/executor/service/service/ExecutionService.java
@@ -2,54 +2,11 @@ package executor.service.service;
 
 import executor.service.model.ProxyConfigHolder;
 import executor.service.model.Scenario;
-import executor.service.model.WebDriverConfig;
-import executor.service.service.impl.WebDriverInitializerImpl;
-import org.openqa.selenium.WebDriver;
 
-import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 
-/**
- * The facade for execute ScenarioExecutor.
- *
- *  @author Oleksandr Tuleninov
- *  @version 01
- * */
-public class ExecutionService {
+public interface ExecutionService {
 
-    private final ScenarioExecutor scenarioExecutor;
-    private final WebDriverConfig webDriverConfig;
+    void execute(BlockingQueue<Scenario> scenarios, BlockingQueue<ProxyConfigHolder> proxies);
 
-    public ExecutionService(ScenarioExecutor scenarioExecutor,
-                            WebDriverConfig webDriverConfig) {
-        this.scenarioExecutor = scenarioExecutor;
-        this.webDriverConfig = webDriverConfig;
-    }
-
-    /**
-     * Execute ScenarioExecutor.
-     *
-     * @param scenarios the queue with scenarios
-     * @param proxies   the queue with proxies
-     */
-    public void execute(Queue<Scenario> scenarios, Queue<ProxyConfigHolder> proxies) {
-        for (Scenario scenario : scenarios) {
-            ProxyConfigHolder proxy = proxies.poll();
-            if (scenario == null || proxy == null) continue;
-
-            WebDriver webDriver = getWebDriverPrototype(webDriverConfig, proxy);
-            if (webDriver == null) continue;
-
-            scenarioExecutor.execute(scenario, webDriver);
-        }
-    }
-
-    /**
-     * Get WebDriver as Prototype.
-     *
-     * @param webDriverConfig   the WebDriverConfig entity
-     * @param proxy the ProxyConfigHolder entity
-     */
-    private WebDriver getWebDriverPrototype(WebDriverConfig webDriverConfig, ProxyConfigHolder proxy) {
-        return new WebDriverInitializerImpl().getInstance(webDriverConfig, proxy);
-    }
 }
---
+++ src/main/java/executor/service/service/impl/ExecutionServiceImpl.java
@@ -0,0 +1,69 @@
+package executor.service.service.impl;
+
+import executor.service.model.ProxyConfigHolder;
+import executor.service.model.Scenario;
+import executor.service.model.WebDriverConfig;
+import executor.service.service.ExecutionService;
+import executor.service.service.ScenarioExecutor;
+import executor.service.service.WebDriverInitializer;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * The facade for execute ScenarioExecutor.
+ *
+ *  @author Oleksandr Tuleninov
+ *  @version 01
+ * */
+public class ExecutionServiceImpl implements ExecutionService {
+
+    private static final Logger log = LoggerFactory.getLogger(ExecutionServiceImpl.class);
+
+    private final ScenarioExecutor scenarioExecutor;
+    private final WebDriverConfig webDriverConfig;
+
+    public ExecutionServiceImpl(ScenarioExecutor scenarioExecutor,
+                                WebDriverConfig webDriverConfig) {
+        this.scenarioExecutor = scenarioExecutor;
+        this.webDriverConfig = webDriverConfig;
+    }
+
+    /**
+     * Execute ScenarioExecutor.
+     *
+     * @param scenarios the queue with scenarios
+     * @param proxies   the queue with proxies
+     */
+    @Override
+    public void execute(BlockingQueue<Scenario> scenarios,
+                        BlockingQueue<ProxyConfigHolder> proxies) {
+        while (true) {
+            try {
+                Scenario scenario = scenarios.take();
+                ProxyConfigHolder proxy = proxies.take();
+
+                WebDriver webDriver = getWebDriverPrototype(webDriverConfig, proxy);
+                if (webDriver == null) continue;
+
+                scenarioExecutor.execute(scenario, webDriver);
+            } catch (InterruptedException e) {
+                log.info("Thread was interrupted in ExecutionServiceImpl.class");
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    /**
+     * Get WebDriver as Prototype.
+     *
+     * @param webDriverConfig   the WebDriverConfig entity
+     * @param proxyConfigHolder the ProxyConfigHolder entity
+     */
+    private WebDriver getWebDriverPrototype(WebDriverConfig webDriverConfig, ProxyConfigHolder proxyConfigHolder) {
+        WebDriverInitializer webDriverInitializer = new WebDriverInitializerImpl();
+        return webDriverInitializer.getInstance(webDriverConfig, proxyConfigHolder);
+    }
+}
