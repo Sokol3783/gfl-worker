a65d20956236d229f56c4132844f296a40a2d2fb
Sokol3783 <sokolplus3783@gmail.com>
2023-09-07 13:01:22 +0300

    add  StepExecution's implementations

--- src/main/java/executor/service/service/impl/StepExecutionClickCssImpl.java
+++ src/main/java/executor/service/service/impl/StepExecutionClickCssImpl.java
@@ -1,17 +1,22 @@
 package executor.service.service.impl;
 
 import executor.service.model.Step;
+import executor.service.model.StepTypes;
 import executor.service.service.StepExecutionClickCss;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class StepExecutionClickCssImpl implements StepExecutionClickCss {
-    @Override
-    public String getStepAction() {
-        return null;
-    }
 
-    @Override
-    public void step(WebDriver webDriver, Step step) {
+  @Override
+  public String getStepAction() {
+    return StepTypes.CLICK_CSS.getName();
+  }
 
-    }
+  @Override
+  public void step(WebDriver webDriver, Step step) {
+    WebElement element = webDriver.findElement(By.cssSelector(step.getValue()));
+    element.click();
+  }
 }
--- src/main/java/executor/service/service/impl/StepExecutionSleepImpl.java
+++ src/main/java/executor/service/service/impl/StepExecutionSleepImpl.java
@@ -1,17 +1,40 @@
 package executor.service.service.impl;
 
+import executor.service.exceptions.StepExecutionException;
 import executor.service.model.Step;
+import executor.service.model.StepTypes;
 import executor.service.service.StepExecutionSleep;
+import java.util.Random;
 import org.openqa.selenium.WebDriver;
 
 public class StepExecutionSleepImpl implements StepExecutionSleep {
-    @Override
-    public String getStepAction() {
-        return null;
+
+  @Override
+  public String getStepAction() {
+    return StepTypes.SLEEP.getName();
+  }
+
+  @Override
+  public void step(WebDriver webDriver, Step step) {
+    try {
+      Thread.sleep(getRandom(step.getValue()));
+    } catch (InterruptedException e) {
+      throw new StepExecutionException(e);
     }
 
-    @Override
-    public void step(WebDriver webDriver, Step step) {
+  }
 
-    }
+  private int getRandom(String value) {
+
+    String values[] = value.split(":");
+    Random random = new Random();
+    int min = Integer.parseInt(values[0]);
+    int max = Integer.parseInt(values[values.length - 1]);
+
+    return random.ints(min, max + 1)
+        .findFirst().orElse(1);
+
+  }
+
+
 }
