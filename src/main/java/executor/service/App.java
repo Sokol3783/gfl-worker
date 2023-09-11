8926ef509bf563556d2a321633e8900a365ab88b
Sokol3783 <sokolplus3783@gmail.com>
2023-09-11 20:54:54 +0300

    add catch clause for creating objects in ObjectFactory

--- src/main/java/executor/service/App.java
+++ src/main/java/executor/service/App.java
@@ -1,18 +1,8 @@
 package executor.service;
 
-import executor.service.DIFactory.ObjectFactoryImpl;
-import executor.service.service.StepExecutionClickCss;
-
 public class App {
 
-  public static void main(String[] args)
-      throws Throwable {
+  public static void main(String[] args) {
 
-    ObjectFactoryImpl factory = ObjectFactoryImpl.getInstance();
-    StepExecutionClickCss stepExecutionClickCss1 = factory.create(StepExecutionClickCss.class);
-    StepExecutionClickCss stepExecutionClickCss2 = factory.create(StepExecutionClickCss.class);
-    System.out.println(stepExecutionClickCss1.getStepAction());
-    System.out.println(stepExecutionClickCss2.getStepAction());
-    System.out.println(stepExecutionClickCss1.equals(stepExecutionClickCss2));
   }
 }
--- src/main/java/executor/service/DIFactory/ObjectFactoryImpl.java
+++ src/main/java/executor/service/DIFactory/ObjectFactoryImpl.java
@@ -1,7 +1,5 @@
 package executor.service.DIFactory;
 
-import org.reflections.Reflections;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -9,73 +7,88 @@ import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import org.reflections.Reflections;
 
 public class ObjectFactoryImpl implements ObjectFactory {
-    private static final Singleton INSTANCE = Singleton.INSTANCE;
 
-    private volatile static ObjectFactoryImpl instance = null;
-    private ObjectFactoryImpl() {}
-    public static ObjectFactoryImpl getInstance() {
+  private static final Singleton INSTANCE = Singleton.INSTANCE;
+
+  private volatile static ObjectFactoryImpl instance = null;
+
+  private ObjectFactoryImpl() {
+  }
+
+  public static ObjectFactoryImpl getInstance() {
+    if (instance == null) {
+      synchronized (ObjectFactoryImpl.class) {
         if (instance == null) {
-            synchronized(ObjectFactoryImpl.class) {
-                if (instance == null) {
-                    instance = new ObjectFactoryImpl();
-                }
-            }
+          instance = new ObjectFactoryImpl();
         }
-        return instance;
+      }
     }
+    return instance;
+  }
+
+  @Override
+  public <T> T create(Class<T> clazz) {
+    return INSTANCE.create(clazz);
+  }
+
+  private enum Singleton implements ObjectFactory {
+    INSTANCE;
+    private static final Map<Class, Object> context = new ConcurrentHashMap<>();
+
+    private final Reflections scanner = ClassScannerUtil.getClassScanner("executor.service");
 
     @Override
-    public <T> T create(Class<T> clazz) throws InstantiationException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        return INSTANCE.create(clazz);
+    public <T> T create(Class<T> clazz) {
+      try {
+        Object object = context.get(clazz);
+        if (object == null) {
+          if (clazz.isInterface()) {
+            Set<Class<? extends T>> subTypesOf = scanner.getSubTypesOf(clazz);
+            if (subTypesOf.iterator().hasNext()) {
+              object = createInstance(subTypesOf.iterator().next());
+              context.put(clazz, object);
+            } else {
+              throw new InstantiationException("No implementation of class");
+            }
+          } else {
+            object = createInstance(clazz);
+            context.put(clazz, object);
+          }
+        }
+        return (T) object;
+      } catch (InvocationTargetException e) {
+        throw new RuntimeException(e);
+      } catch (NoSuchMethodException e) {
+        throw new RuntimeException(e);
+      } catch (InstantiationException e) {
+        throw new RuntimeException(e);
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
+      }
     }
 
-    private enum Singleton implements ObjectFactory {
-        INSTANCE;
-        private static final Map<Class,Object> context = new ConcurrentHashMap<>();
+    private Object createInstance(Class clazz)
+        throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+      Constructor declaredConstructor = clazz.getDeclaredConstructor();
+      Object o = declaredConstructor.newInstance();
+      if (declaredConstructor.getParameterCount() > 0) {
+        return o;
+      }
 
-        private final Reflections scanner = ClassScannerUtil.getClassScanner("executor.service");
-
-        @Override
-        public <T> T create(Class<T> clazz) throws InstantiationException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-            Object object = context.get(clazz);
-            if (object == null) {
-                if (clazz.isInterface()) {
-                    Set<Class<? extends T>> subTypesOf = scanner.getSubTypesOf(clazz);
-                    if (subTypesOf.iterator().hasNext()) {
-                        object = createInstance(subTypesOf.iterator().next());
-                        context.put(clazz, object);
-                    } else {
-                        throw new InstantiationException("No implementation of class");
-                    }
-                } else {
-                    object = createInstance(clazz);
-                    context.put(clazz, object);
-                }
-            }
-            return (T) object;
+      for (Type type : declaredConstructor.getParameterTypes()) {
+        Field[] declaredFields = o.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+          if (type.equals(field.getType())) {
+            field.setAccessible(true);
+            field.set(field, create(type.getClass()));
+          }
         }
-
-        private Object createInstance(Class clazz)
-                throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-            Constructor declaredConstructor = clazz.getDeclaredConstructor();
-            Object o = declaredConstructor.newInstance();
-            if (declaredConstructor.getParameterCount() > 0) {
-                return o;
-            }
-
-            for (Type type : declaredConstructor.getParameterTypes()) {
-                Field[] declaredFields = o.getClass().getDeclaredFields();
-                for (Field field : declaredFields) {
-                    if (type.equals(field.getType())) {
-                        field.setAccessible(true);
-                        field.set(field, create(type.getClass()));
-                    }
-                }
-            }
-            return o;
-        }
+      }
+      return o;
     }
+  }
 
 }
