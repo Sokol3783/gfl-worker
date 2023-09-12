98be1115eaae955947f9aa60a9062e6345c2145e
Olexandr Tuleninov <talextu1@gmail.com>
2023-09-12 14:17:25 +0300

    added JavaDoc to ItemHandler.java

--- src/main/java/executor/service/service/ItemHandler.java
+++ src/main/java/executor/service/service/ItemHandler.java
@@ -1,5 +1,11 @@
 package executor.service.service;
 
+/**
+ * Start ExecutionService in parallel multi-threaded mode.
+ *
+ * @author Oleksandr Tuleninov
+ * @version 01
+ */
 public interface ItemHandler<T> {
 
     void onScenarioReceived(T item);
