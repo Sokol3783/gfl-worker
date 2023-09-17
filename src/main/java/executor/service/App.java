36cbe1bdd9a0f166fa128ba7eaf055eacb6d48c4
Sokol3783 <sokolplus3783@gmail.com>
2023-09-17 14:36:48 +0300

    fix main App class

--- src/main/java/executor/service/App.java
+++ src/main/java/executor/service/App.java
@@ -4,8 +4,5 @@ public class App {
 
   public static void main(String[] args) {
 
-    private static String getPackageName() {
-        Package currentPackage = App.class.getPackage();
-        return currentPackage.getName();
-    }
+  }
 }
