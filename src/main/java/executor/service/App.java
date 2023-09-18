18ac9d1c84540d889c7f5ba26dcccbb3985482da
ykotsiuba <jurok3x@gmail.com>
2023-09-18 16:35:32 +0300

    Removed unused imports.

--- src/main/java/executor/service/App.java
+++ src/main/java/executor/service/App.java
@@ -1,13 +1,5 @@
 package executor.service;
 
-import executor.service.config.di.FactoryForDI;
-import executor.service.config.di.ApplicationContext;
-import executor.service.service.ParallelFlowExecutorService;
-import executor.service.service.parallel.ParallelFlowExecutorServiceImpl;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
 
     public static void main( String[] args ) {
