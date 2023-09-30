a061006c506efaf8371379fe1d4c27329d3c2b17
Dima (IfLunatic) <dimasilenkoluna@gmail.com>
2023-09-30 18:59:14 +0300

    changes contract of interfaces

--- src/main/java/executor/service/service/stepexecution/StepExecutionClickXpath.java
+++ src/main/java/executor/service/service/stepexecution/StepExecutionClickXpath.java
@@ -1,11 +1,14 @@
 package executor.service.service.stepexecution;
 
 import executor.service.model.scenario.Step;
+import executor.service.model.scenario.StepTypes;
 import org.openqa.selenium.WebDriver;
 
 public interface StepExecutionClickXpath extends StepExecution{
 
-    String getStepAction();
+    String getNameStepAction();
+
+    StepTypes stepAction();
 
     void step(WebDriver webDriver, Step step);
 
--- src/main/java/executor/service/service/stepexecution/StepExecutionFabric.java
+++ src/main/java/executor/service/service/stepexecution/StepExecutionFabric.java
@@ -1,7 +1,12 @@
 package executor.service.service.stepexecution;
 
+import executor.service.model.scenario.StepTypes;
+import executor.service.service.stepexecution.impl.StepExecutionFabricimpl;
+
 public interface StepExecutionFabric {
 
    StepExecution getStepExecutor(String stepAction);
+
+   StepExecution getStepExecutor(StepTypes stepAction);
 
 }
