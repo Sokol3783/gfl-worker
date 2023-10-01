8b3d0ee019b0d048e4052d6a1a3d0d97333da7ce
ykotsiuba <jurok3x@gmail.com>
2023-10-01 14:58:59 +0300

    Added javadocs to provider classes.

--- src/main/java/executor/service/service/parallelflowexecutor/impls/publishers/ScenarioPublisher.java
+++ src/main/java/executor/service/service/parallelflowexecutor/impls/publishers/ScenarioPublisher.java
@@ -5,15 +5,38 @@ import executor.service.queue.ScenarioQueue;
 import executor.service.service.parallelflowexecutor.Task;
 import executor.service.service.scenarios.ScenarioSourceListener;
 
+/**
+ * The {@code ScenarioPublisher} class is responsible for publishing scenarios to a scenario queue
+ * by utilizing a provided {@link ScenarioSourceListener}.
+ * <p>
+ * This class implements the {@link Task} interface, allowing it to be executed in a separate
+ * thread or context.
+ * <p>
+ *
+ * @author Yurii Kotsiuba
+ * @see Task
+ * @see ScenarioQueue
+ * @see ScenarioSourceListener
+ */
 public class ScenarioPublisher implements Task {
     private final ScenarioQueue scenarioQueue;
     private final ScenarioSourceListener listener;
 
+    /**
+     * Constructs a new {@code ScenarioPublisher} with the specified scenario queue and listener.
+     *
+     * @param scenarioQueue The scenario queue to which scenarios will be published.
+     * @param listener      The listener responsible for executing scenario publication.
+     */
     public ScenarioPublisher(ScenarioQueue scenarioQueue, ScenarioSourceListener listener) {
         this.scenarioQueue = scenarioQueue;
         this.listener = listener;
     }
 
+    /**
+     * Runs the scenario publication process by invoking the execute
+     * method with the scenario queue's {@code addScenario} method reference.
+     */
     @Override
     public void run() {
         listener.execute(scenarioQueue::addScenario);
