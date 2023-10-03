6ff0456a33060966567a71254a24a5b6e8e467e5
Olexandr Tuleninov <talextu1@gmail.com>
2023-10-03 11:08:47 +0300

    refactored ExecutionServiceImpl.java (dropped checking for null in public void execute(Scenario scenario, ProxyConfigHolder proxy), added JavaDoc)

--- src/main/java/executor/service/service/executionservice/ExecutionServiceImpl.java
+++ src/main/java/executor/service/service/executionservice/ExecutionServiceImpl.java
@@ -5,32 +5,52 @@ import executor.service.model.scenario.Scenario;
 import executor.service.service.scenarios.ScenarioExecutor;
 import executor.service.service.webdriver.WebDriverInitializer;
 import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-
-//TODO
+/**
+ * Implementation of the {@link ExecutionService} interface.
+ * This class orchestrates the execution of scenarios using a provided
+ * {@link ScenarioExecutor} and initializes {@link WebDriver} instances using
+ * a {@link WebDriverInitializer}.
+ * <p>
+ *
+ * @author Oleksandr Tuleninov
+ * @version 01
+ * @see executor.service.service.scenarios.ScenarioExecutor
+ * @see executor.service.service.webdriver.WebDriverInitializer
+ */
 public class ExecutionServiceImpl implements ExecutionService {
-    private static final Logger log = LoggerFactory.getLogger(ExecutionServiceImpl.class);
 
     private final ScenarioExecutor scenarioExecutor;
     private final WebDriverInitializer initializer;
 
-    public ExecutionServiceImpl(ScenarioExecutor scenarioExecutor, WebDriverInitializer initializer) {
+    public ExecutionServiceImpl(ScenarioExecutor scenarioExecutor,
+                                WebDriverInitializer initializer) {
         this.scenarioExecutor = scenarioExecutor;
         this.initializer = initializer;
     }
 
+    /**
+     * Executes a given scenario using a {@link WebDriver} instance and a
+     * provided {@link ProxyConfigHolder}.
+     *
+     * @param scenario the scenario
+     * @param proxy    the proxy
+     */
     @Override
     public void execute(Scenario scenario, ProxyConfigHolder proxy) {
         WebDriver webDriver = getWebDriver(proxy);
-        if (webDriver == null) {
-            log.error("Web-driver initialization error");
-            return;
-        }
+
         scenarioExecutor.execute(scenario, webDriver);
     }
 
+    /**
+     * Retrieves a {@link WebDriver} instance using the provided
+     * {@link ProxyConfigHolder}.
+     *
+     * @param proxyConfigHolder The {@link ProxyConfigHolder} entity containing
+     *                          proxy configuration information.
+     * @return The initialized {@link WebDriver} instance.
+     */
     private WebDriver getWebDriver(ProxyConfigHolder proxyConfigHolder) {
         return initializer.getInstance(proxyConfigHolder);
     }
