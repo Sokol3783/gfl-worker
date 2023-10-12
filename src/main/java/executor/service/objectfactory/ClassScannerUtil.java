7f6b48095a14b6b937ee1236c5630d859a8e0888
Sokol3783 <sokolplus3783@gmail.com>
2023-10-12 12:26:23 +0300

    fix call of package name

--- src/main/java/executor/service/objectfactory/ClassScannerUtil.java
+++ src/main/java/executor/service/objectfactory/ClassScannerUtil.java
@@ -1,10 +1,11 @@
 package executor.service.objectfactory;
 
+import executor.service.App;
 import org.reflections.Reflections;
 
 class ClassScannerUtil {
 
-  private static Reflections CLASS_SCANNER = new Reflections("/src/java/executor/service/");
+  private static Reflections CLASS_SCANNER = new Reflections(App.class.getPackageName());
 
   public static Reflections getClassScanner(String path) {
     if (path != null && !path.isEmpty()) {
