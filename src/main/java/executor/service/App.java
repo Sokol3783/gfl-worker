1d43b6fccdb785cd2b843bd03e666ad06d301f44
Sokol3783 <sokolplus3783@gmail.com>
2023-10-12 12:30:12 +0300

    add logger to app class and fix runnable function

--- src/main/java/executor/service/App.java
+++ src/main/java/executor/service/App.java
@@ -3,19 +3,20 @@ package executor.service;
 import executor.service.objectfactory.ObjectFactory;
 import executor.service.objectfactory.ObjectFactoryImpl;
 import executor.service.service.parallelflowexecutor.ParallelFlowExecutorService;
+import executor.service.service.scenarios.ScenarioSourceListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
+
+  private static final Logger log = LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) {
 
     ObjectFactory factory = new ObjectFactoryImpl();
     ParallelFlowExecutorService mainService = factory.create(ParallelFlowExecutorService.class);
-    mainService.execute(new Runnable() {
-      @Override
-      public void run() {
-        System.out.println("Worker successfully run!");
-      }
-    });
+    mainService.execute(() -> log.info("Worker successfully run!"));
+
   }
 
 }
