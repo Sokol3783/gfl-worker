67b71eff4bb05b4e8752e7193cf1ce2493d4cca7
Anton Sokolskyi <sokolplus3783@gmail.com>
2023-12-31 14:13:35 +0200

    remove unused import

--- src/main/java/executor/service/queue/ScenarioQueue.java
+++ src/main/java/executor/service/queue/ScenarioQueue.java
@@ -4,7 +4,6 @@ import executor.service.model.scenario.Scenario;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
